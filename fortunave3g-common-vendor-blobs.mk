52cf4afb18030b7070a06c46c76b4af86d12d539
ZIM <zim555@mail.ru>
2016-03-26 20:13:19 +0200

    Begin vendor blob

--- fortunave3g-common-vendor-blobs.mk
+++ fortunave3g-common-vendor-blobs.mk
@@ -1,1 +1,16 @@
 LOCAL_PATH := vendor/samsung/fortunave3g/proprietary
+
+# Ril
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bin/rild:system/bin/rild \
+	$(LOCAL_PATH)/lib/libril.so:system/lib/libril.so \
+	$(LOCAL_PATH)/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
+	$(LOCAL_PATH)/lib/libsec-ril.so:system/lib/libsec-ril.so \
+    $(LOCAL_PATH)/lib/libsec-ril-dsds.so:system/lib/libsec-ril-dsds.so \
+	$(LOCAL_PATH)/lib/libsecril-client.so:system/lib/libsecril-client.so \
+    $(LOCAL_PATH)/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
+	$(LOCAL_PATH)/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so
+	
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vendor/lib/libHevcSwDecoder.so:system/vendor/lib/libHevcSwDecoder.so \
+	$(LOCAL_PATH)/vendor/lib/libmm-abl-oem.so:system/vendor/lib/libmm-abl-oem.so
--- fortunave3g-common-vendor.mk
+++ fortunave3g-common-vendor.mk
@@ -1,5 +1,18 @@
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fortunave3g/overlay
 
+PRODUCT_PACKAGES += \
+    com.qualcomm.location
+
+PRODUCT_PACKAGES += \
+    libtime_genoff \
+    TimeService
+
+PRODUCT_PACKAGES += \
+    libmm-abl
+
+PRODUCT_PACKAGES += \
+    libHevcSwDecoder
+
 # Include non-open-source files
 $(call inherit-product, vendor/samsung/fortunave3g/fortunave3g-common-vendor-blobs.mk)
